4c8fbfda7a682f66cc2bfb16ae0ac34f6eafd281
gabrielm2q <mariano.gabriel2q@gmail.com>
2024-09-18 22:06:16 -0300

    :sparkles: implements mutual initial communication
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>

--- src/CommunicatorAgent.java
+++ src/CommunicatorAgent.java
@@ -1,6 +1,8 @@
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -11,6 +13,8 @@ import jade.lang.acl.MessageTemplate;
 
 public class CommunicatorAgent extends Agent {
 	private static final long serialVersionUID = 1L;
+	private AID[] communicatorAgents;
+	private boolean mayIStart = false;
 	
 	protected void setup () {
 		DFAgentDescription dfd = new DFAgentDescription();
@@ -19,7 +23,7 @@ public class CommunicatorAgent extends Agent {
 		sd.setType("communicator");
 		sd.setName("p2p-communicator");
 		dfd.addServices(sd);	
-		System.out.println("Mensagem recebida: " + this.getName() + " e "+ this.getLocalName());
+		System.out.println("Eu sou: " + this.getName() + " e "+ this.getLocalName());
 
 		try {
 			DFService.register(this, dfd);
@@ -28,24 +32,21 @@ public class CommunicatorAgent extends Agent {
 			fe.printStackTrace();
 		}
 		
-		addBehaviour(new ReceiveMessage());
-		
+		addBehaviour(new ReceiveInitialMessage());
 	}
 	
-	private class ReceiveMessage extends CyclicBehaviour {
-		
+	private class ReceiveInitialMessage extends CyclicBehaviour {
 		private static final long serialVersionUID = 1L;
 		
 		public void action () {
 			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 			
 			ACLMessage msg = myAgent.receive(mt);
-			
+					
 			if ( msg != null ) {
-				
 				String title = msg.getContent();
 				
-				System.out.println("Mensagem recebida: " + title);
+				System.out.println("Mensagem inicial recebida: " + title);
 				
 				ACLMessage reply = msg.createReply();
 				
@@ -53,9 +54,97 @@ public class CommunicatorAgent extends Agent {
 				
 				myAgent.send(reply);
 				
+				mayIStart = true;
+				
+				addBehaviour(new SendMessage());
 			}
 			
 		}
+	}
+	
+	private class ReceiveMessage extends CyclicBehaviour {
 		
+		private static final long serialVersionUID = 1L;
+		
+		public void action () {
+			while ( !mayIStart ) {
+				try {
+					Thread.sleep(2500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+			
+			ACLMessage msg = myAgent.receive(mt);
+					
+			if ( msg != null ) {
+				String title = msg.getContent();
+				
+				System.out.println("Mensagem de Hello recebida pelo " + myAgent.getName() + ": " + title);
+				
+//				ACLMessage reply = msg.createReply();
+//				
+//				reply.setPerformative(ACLMessage.CONFIRM);
+//				
+//				myAgent.send(reply);
+			}
+			
+		}
+	}
+	
+	private class SendMessage extends OneShotBehaviour {
+		private static final long serialVersionUID = 1L;
+		
+		public void action () {
+			while ( !mayIStart ) {
+				try {
+					Thread.sleep(2500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			DFAgentDescription template = new DFAgentDescription();
+			ServiceDescription sd2 = new ServiceDescription();
+			sd2.setType("communicator");
+			template.addServices(sd2);
+			
+			try {
+				DFAgentDescription[] result = DFService.search(myAgent, template);
+				if ( result.length > 0 ) {
+					System.out.println("ACHEI!" + result.toString());
+				} else {
+					System.out.println("Não achei...");
+				}
+				communicatorAgents = new AID[result.length + 1];
+				
+				for (int i = 0; i < result.length; ++i) {
+					communicatorAgents[i] = result[i].getName();
+					System.out.println("ENCONTREI: " + communicatorAgents[i].getName());
+				}
+			}
+			catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+			
+			
+			ACLMessage helloMessage = new ACLMessage(ACLMessage.INFORM);
+			
+			helloMessage.setContent("Hey there!");
+			helloMessage.setConversationId("communicator");
+			helloMessage.setContent("Hello from: " + myAgent.getName());
+			
+			for ( AID agent : communicatorAgents ) {
+				if ( agent != myAgent.getAID() ) {
+					helloMessage.addReceiver(agent);
+				}
+			}
+			
+			addBehaviour(new ReceiveMessage());
+			
+			myAgent.send(helloMessage);
+		}
 	}
 }
